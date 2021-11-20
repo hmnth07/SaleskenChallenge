3e5a74f1bc59baade3e9ac053136171f9260325f
Hemanth <60520439+hmnth07@users.noreply.github.com>
2021-11-20 18:59:24 +0530

    Adding files

---
+++ readme.md
@@ -0,0 +1,17 @@
+Music Copy rights: https://www.bensound.com
+
+
+SalesKen Challenge
+Requirements:
+
+1. Make a static player with play/pause control (no need of fast forward or audio control) // done
+
+2. The bars height and direction can be random but need to have a maximum height. //
+
+3. Play Button Click:Audio should play and start moving the audio player. //done
+
+4. Also click anywhere on the player and it should move to the exact time of the audio(like in the video above). //done
+
+5. Add static tags exactly as shown in the above video. //
+
+6. Pause Button Click : Should stop playing the audio. //done
---
+++ script.js
@@ -0,0 +1,64 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+
+// Play song
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+  
+    audio.play();
+}
+  
+// Pause song
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+  
+    audio.pause();
+}
+
+// Update progress bar
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+  }
+  
+// Set progress bar
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+  
+    audio.currentTime = (clickX / width) * duration;
+}
+
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+  
+    if (isPlaying) {
+      pauseSong();
+    } else {
+      playSong();
+    }
+});
+
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', pauseSong);
